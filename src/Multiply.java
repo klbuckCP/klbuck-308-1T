0db30b31e5b9b330c2e74d2cd421e5aef101af59
klbuckCP <klbuck@calpoly.edu>
2019-02-06 10:29:45 -0800

    adding files

---
+++ src/Multiply.java
@@ -0,0 +1,9 @@
+public class Multiply {
+	
+	public static int multMethod(int a, int b) {
+		return a * b;
+	}
+	public static void main(String[] args) {
+		System.out.println(multMethod(1, 3));
+    }
+}
